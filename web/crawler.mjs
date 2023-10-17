29dd7d8d8ee9dc7e03437ab12cffb2151f362c50
Hao Cui <cuih7@uci.edu>
2023-10-17 09:22:05 -0700

    crawler: implementation WIP

--- web/crawler.mjs
+++ web/crawler.mjs
@@ -1,6 +1,10 @@
 import { chromium } from 'playwright-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { PlaywrightBlocker } from '@cliqz/adblocker-playwright';
+import process from 'node:process';
+import mnemonist from 'mnemonist';
+
+const { Heap } = mnemonist;
 
 /**
  * Guess the type of a form
@@ -68,12 +72,56 @@ async function checkFieldType(formLocator, fieldLocator) {
   return 'UNKNOWN';
 }
 
+/**
+ * Estimate the reward of clicking an element
+ *
+ * @param {import('playwright').Locator} locator
+ * @returns {Promise<number>}
+ */
+async function estimateClickReward(locator) {
+  const textContent = await locator.textContent();
+
+  if (textContent.search(/\b(sign|create|forgot|reset|register|new|log|setting)s?\b/gi)) {
+    return 500;
+  }
+
+  return 0;
+}
+
+/**
+ * Sleep :)
+ *
+ * @param {number} ms
+ * @returns {Promise<any>}
+ */
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 (async () => {
   // TODO:
   //   - click buttons / links to discover forms
   //   - detect popup dialogs
   //   - discover privacy policy links
 
+  const landingURLs = process.argv.slice(2);
+
+  const jobQueue = new Heap((job1, job2) => {
+    let retVal = Math.sign(job2.priority - job1.priority);
+    retVal = retVal === 0 ? Math.sign(job2.ts - job1.ts) : retVal;
+    return retVal;
+  });
+
+  for (const url of landingURLs) {
+    jobQueue.push({
+      priority: 1000,
+      ts: new Date().getTime(),
+      actions: [],
+      url,
+    });
+  }
+
+  // Initialize the browser
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
@@ -82,32 +130,79 @@ async function checkFieldType(formLocator, fieldLocator) {
     blocker.enableBlockingInPage(page);
   });
 
-  // Stealth plugin - not sure if it acutall helps but why not
+  // Stealth plugin - not sure if it actually helps but why not
   chromium.use(StealthPlugin());
 
-  await page.goto('https://www.facebook.com/', { waitUntil: 'networkidle' });
+  // Main loop
+  while (jobQueue.size > 0) {
+    const job = jobQueue.pop();
+    console.log('Current job: ', job);
 
-  await page.getByText('Create new account').click();
-  await page.waitForTimeout(2000);
-  await page.waitForLoadState('networkidle');
-  await page.screenshot({ path: 'screenshot.png' });
+    await page.goto(job.url, { waitUntil: 'networkidle' });
 
-  const formLocator = page.locator('form');
+    for (const actionDesc of job.actions) {
+      // Do the actions
+      const { _: action, ...elementDesc } = actionDesc;
 
-  for (const form of await formLocator.all()) {
-    await form.scrollIntoViewIfNeeded();
+      console.log('Action', action);
+      console.log('Element:', elementDesc);
 
-    const formType = await checkFormType(form);
-
-    console.log(`FORM TYPE: ${formType}`);
-
-    for (const inputField of await form.locator('input').all()) {
-      if (await inputField.isVisible()) {
-        const fieldType = await checkFieldType(form, inputField);
-        console.log(`- FIELD TYPE: ${fieldType}`);
+      if (action === 'click') {
+        await page.getByRole('button')
+          .or(page.getByRole('link'))
+          .filter(elementDesc)
+          .click();
       }
     }
+
+    // Search the webpage for forms
+    for (const form of await page.locator('form').all()) {
+      await form.scrollIntoViewIfNeeded();
+
+      const formType = await checkFormType(form);
+
+      console.log(`FORM TYPE: ${formType}`);
+
+      for (const inputField of await form.locator('input').all()) {
+        if (await inputField.isVisible()) {
+          const fieldType = await checkFieldType(form, inputField);
+          console.log(`- FIELD TYPE: ${fieldType}`);
+        }
+      }
+    }
+
+    // Identify possible next steps
+    const locator = page.getByRole('button').or(page.getByRole('link'));
+    for (const element of await locator.all()) {
+      await element.scrollIntoViewIfNeeded();
+
+      const clickReward = await estimateClickReward(element);
+      const newActions = job.actions.slice();
+
+      newActions.push({
+        _: 'click',
+        hasText: await element.textContent(),
+      });
+
+      const newJobDesc = {
+        priority: clickReward,
+        ts: new Date().getTime(),
+        actions: newActions,
+        url: job.url,
+      };
+
+      jobQueue.push(newJobDesc);
+
+      //console.log('New job: ', newJobDesc);
+    }
+
+    sleep(2000); // For now, avoid running too fast
   }
+
+  // Log file
+  // root/<URL>/<step-hash>/html
+
+
 
   await browser.close();
 })();
